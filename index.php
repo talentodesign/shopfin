7fc89826a15394b08a363ca09c7f740937adde84
Genival dos Santos Furtado Junior <moreno.alado@gmail.com>
2013-04-24 10:28:40 -0300

    COMMIT INICIAL

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include("header.php") ?>
+    <title><?php echo $core_nome; ?></title>
+  </head>
+  <body>
+    
+    <div id="content">
+    </div>
+    <div id="navigation">
+    </div>
+    <div id="sidebar">
+    </div>
+    <div id="footer">
+    <?php $data=date("Y"); echo $data; ?> <!-- Data Atual -->
+    </div>
+  </body>
+</html>
+
